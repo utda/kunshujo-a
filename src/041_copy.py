6087652f72e5b309993fd266fa12284e380b9bfc
nakamura196 <na.kamura.1263@gmail.com>
2021-12-08 23:09:01 +0900

    feat: initial commit

---
+++ src/041_copy.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import urllib.parse
+import json
+import copy
+import os
+import hashlib
+import datetime
+import itertools
+
+import shutil
+
+
+shutil.copytree("/Users/nakamurasatoru/git/d_omeka/omekac_dd/docs/files/medium", "/Users/nakamurasatoru/git/d_kunshujo/kunshujo-a/docs/files/medium")
+shutil.copytree("/Users/nakamurasatoru/git/d_omeka/omekac_dd/docs/pd", "/Users/nakamurasatoru/git/d_kunshujo/kunshujo-a/docs/pd")
+shutil.copytree("/Users/nakamurasatoru/git/d_omeka/omekac_dd/docs/pp", "/Users/nakamurasatoru/git/d_kunshujo/kunshujo-a/docs/pp")
+shutil.copytree("/Users/nakamurasatoru/git/d_omeka/omekac_dd/docs/iiif", "/Users/nakamurasatoru/git/d_kunshujo/kunshujo-a/docs/iiif")
